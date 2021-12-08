fdc06d3e263513e10ea5e4ad4e7a4bba221aa899
鲈鱼 <luyu@gegejia.com>
2021-12-08 15:08:34 +0800

    二维数组的查找

--- java/jian-zhi-offer/二维数组中的查找/Solution.java
+++
@@ -1,7 +0,0 @@
-public class Solution {
-
-    public boolean Find(int target, int [][] array) {
-
-    }
-
-}
---
+++ java/src/jianZhiOffer/Array_Find_Solution.java
@@ -0,0 +1,40 @@
+package jianZhiOffer;
+
+/**
+ * 二维数组中的查找
+ */
+public class Array_Find_Solution {
+
+    public boolean Find(int target, int [][] array) {
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            return false;
+        }
+
+        int hIdx = 0, lIdx = array[0].length - 1;
+        int val;
+        while (true) {
+            val = array[hIdx][lIdx];
+            if (val == target) {
+                return true;
+            }
+            if (val < target) {
+                hIdx += 1;
+                if (hIdx > array.length - 1) {
+                    break;
+                }
+            } else if (val > target) {
+                lIdx -= 1;
+                if (lIdx < 0) {
+                    break;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
