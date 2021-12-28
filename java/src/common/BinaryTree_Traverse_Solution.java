ef0f6185098af0622f645d1200e25281e242f334
鲈鱼 <luyu@gegejia.com>
2021-12-28 17:16:00 +0800

    BinaryTree_Traverse_Solution

--- java/src/common/BinaryTree_Traverse_Solution.java
+++ java/src/common/BinaryTree_Traverse_Solution.java
@@ -3,13 +3,18 @@ package common;
 import utils.LogUtils;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
 
 /**
- * 给定一棵二叉树，分别按照二叉树先序，中序和后序打印所有的节点
+ * 给定一棵二叉树，分别按照二叉树先序（根左右），中序（左根右）和后序（左右根）打印所有的节点
+ * 薄弱点：用栈数据结构模拟递归调用方面较弱
  * @author dongxu.lu
  * @date 2021/12/21
  */
 public class BinaryTree_Traverse_Solution {
+
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
 
     /**
      *
@@ -30,7 +35,7 @@ public class BinaryTree_Traverse_Solution {
 
         ArrayList<Integer> arrList3 = new ArrayList<>();
         if (node != null) {
-            afterOrders_Recurse(arrList3, node);
+            postOrders_Recurse(arrList3, node);
         }
 
         int[][] result = new int[3][arrList1.size()];
@@ -43,41 +48,110 @@ public class BinaryTree_Traverse_Solution {
         return result;
     }
 
-    private static void preOrders_Recurse(ArrayList<Integer> intArr, TreeNode node) {
-        intArr.add(node.val);
+    private static void preOrders_Recurse(ArrayList<Integer> arrList, TreeNode node) {
+        arrList.add(node.val);
 
         if (node.left != null) {
-            preOrders_Recurse(intArr, node.left);
+            preOrders_Recurse(arrList, node.left);
         }
         if (node.right != null) {
-            preOrders_Recurse(intArr, node.right);
+            preOrders_Recurse(arrList, node.right);
         }
     }
 
-    private static void middleOrders_Recurse(ArrayList<Integer> intArr, TreeNode node) {
-        if (node.left != null) {
-            middleOrders_Recurse(intArr, node.left);
+    private static void preOrders_OnStack(ArrayList<Integer> arrList, TreeNode node) {
+        if (node == null) {
+            return;
         }
 
-        intArr.add(node.val);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(node);
 
-        if (node.right != null) {
-            middleOrders_Recurse(intArr, node.right);
+        TreeNode currNode;
+        while (!stack.isEmpty()) {
+            currNode = stack.pop();
+            arrList.add(currNode.val);
+
+            if (currNode.right != null) {
+                stack.add(currNode.right);
+            }
+
+            if (currNode.left != null) {
+                stack.add(currNode.left);
+            }
         }
     }
 
-    private static void afterOrders_Recurse(ArrayList<Integer> intArr, TreeNode node) {
+    private static void middleOrders_Recurse(ArrayList<Integer> arrList, TreeNode node) {
         if (node.left != null) {
-            afterOrders_Recurse(intArr, node.left);
+            middleOrders_Recurse(arrList, node.left);
+        }
+
+        arrList.add(node.val);
+
+        if (node.right != null) {
+            middleOrders_Recurse(arrList, node.right);
+        }
+    }
+
+    private static void middleOrders_OnStack(ArrayList<Integer> arrList, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode currNode = node;
+        while (!stack.isEmpty() || currNode != null) {
+            while (currNode != null) {
+                stack.add(currNode);
+                currNode = currNode.left;
+            }
+
+            currNode = stack.pop();
+            arrList.add(currNode.val);
+
+            currNode = currNode.right;
+        }
+    }
+
+    private static void postOrders_Recurse(ArrayList<Integer> arrList, TreeNode node) {
+        if (node.left != null) {
+            postOrders_Recurse(arrList, node.left);
         }
 
         if (node.right != null) {
-            afterOrders_Recurse(intArr, node.right);
+            postOrders_Recurse(arrList, node.right);
         }
 
-        intArr.add(node.val);
+        arrList.add(node.val);
     }
 
+    private static void postOrders_OnStack(ArrayList<Integer> arrList, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+
+        TreeNode currNode = null, preNode = null;
+        while (!stack.isEmpty()) {
+            currNode = stack.peek();
+
+            if ((currNode.left == null && currNode.right == null) // 叶子节点
+                    || (preNode != null && currNode.left == preNode || currNode.right == preNode)) { // 叶子节点的parent
+                arrList.add(currNode.val);
+                preNode = stack.pop();
+            } else {
+                if (currNode.right != null) {
+                    stack.push(currNode.right);
+                }
+                if (currNode.left != null) {
+                    stack.push(currNode.left);
+                }
+            }
+        }
+    }
 
     public static void addLeftRightNodes(TreeNode node, int level) {
         if (level > 3) {
@@ -86,10 +160,10 @@ public class BinaryTree_Traverse_Solution {
         node.val = level * 10 + 1;
 
         node.left = new TreeNode();
-        node.left.val = level * 10 + 3;
+        node.left.val = level * 20 + RANDOM.nextInt(10);
 
         node.right = new TreeNode();
-        node.right.val = level * 10 + 5;
+        node.right.val = level * 30 + RANDOM.nextInt(10);
 
         addLeftRightNodes(node.left, level + 1);
         addLeftRightNodes(node.right, level + 1);
@@ -103,20 +177,50 @@ public class BinaryTree_Traverse_Solution {
 
 
     public static void main(String[] args) {
-        TreeNode node = new TreeNode();
+        TreeNode node;
+        ArrayList<Integer> arrList;
+
+        node = new TreeNode();
         addLeftRightNodes(node, 1);
 
+        arrList = new ArrayList<>();
+        preOrders_Recurse(arrList, node);
+        LogUtils.info("preOrders_Recurse", arrList);
+
+        arrList = new ArrayList<>();
+        preOrders_OnStack(arrList, node);
+        LogUtils.info("preOrders_OnStack", arrList);
+
+        LogUtils.printLine();
+        arrList = new ArrayList<>();
+        middleOrders_Recurse(arrList, node);
+        LogUtils.info("middleOrders_Recurse", arrList);
+
+        arrList = new ArrayList<>();
+        middleOrders_OnStack(arrList, node);
+        LogUtils.info("middleOrders_OnStack", arrList);
+
+        LogUtils.printLine();
+        arrList = new ArrayList<>();
+        postOrders_Recurse(arrList, node);
+        LogUtils.info("postOrders_Recurse", arrList);
+
+        arrList = new ArrayList<>();
+        postOrders_OnStack(arrList, node);
+        LogUtils.info("postOrders_OnStack", arrList);
+
+        LogUtils.printLine();
         int[][] result = threeOrders(node);
         LogUtils.info(result);
 
         result = threeOrders(null);
         LogUtils.info(result);
 
-
         node = new TreeNode();
         node.val = 1;
         result = threeOrders(node);
         LogUtils.info(result);
+
     }
 
 }
--- java/src/utils/LogUtils.java
+++ java/src/utils/LogUtils.java
@@ -10,8 +10,19 @@ public class LogUtils {
 
     private static final Gson GSON = new Gson();
 
-    public static void info(Object object) {
-        System.out.println(GSON.toJson(object));
+    public static void info(Object... objects) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < objects.length; i++) {
+            if (i > 0) {
+                sb.append(" | ");
+            }
+            sb.append(GSON.toJson(objects[i]));
+        }
+        System.out.println(sb);
+    }
+
+    public static void printLine() {
+        System.out.println("--------------------------");
     }
 
 }
