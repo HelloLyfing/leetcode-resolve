7fd1294328b7e47c5cafe5fadb5fb3ee15d9424f
鲈鱼 <luyu@gegejia.com>
2022-03-02 17:48:23 +0800

    add SortingCollection

--- java/src/common/SortingCollection.java
+++ java/src/common/SortingCollection.java
@@ -2,16 +2,16 @@ package common;
 
 import utils.LogUtils;
 
+import java.util.Arrays;
+
 /**
- * 给定一棵二叉树，分别按照二叉树先序（根左右），中序（左根右）和后序（左右根）打印所有的节点
- * 薄弱点：用栈数据结构模拟递归调用方面较弱
  * @author dongxu.lu
  * @date 2021/12/21
  */
 public class SortingCollection {
 
     /**
-     * 快速排序由于排序效率在同为O(N*logN)的几种排序方法中效率较高，因此经常被采用，快速排序思想的分治法也确实实用
+     * 快速排序，时间效率同为O(N*logN)的几种排序方法中效率较高，因此经常被采用，快速排序思想的分治法也确实实用
      *
      * 核心过程：
      * 阶段1：挖坑暂存A，找新值填充老坑，到i=j时归还暂存的A
@@ -59,11 +59,62 @@ public class SortingCollection {
         quickSort(argArr, i + 1, endIdx);
     }
 
+    /**
+     * 归并排序，时间复杂度：O(nlogn), 空间复杂度O(n)
+     * 核心过程：脑子里记下归并排序的图
+     */
+    static class MergeSortRecurse {
+
+        public static void sortAndMerge(int[] origArr, int tempArr[], int beginIdx, int endIdx) {
+            if (beginIdx < endIdx) {
+                int middle = (beginIdx + endIdx) / 2;
+                sortAndMerge(origArr, tempArr, beginIdx, middle);
+                sortAndMerge(origArr, tempArr, middle + 1, endIdx);
+                merge(origArr, tempArr, beginIdx, middle, endIdx);
+            }
+        }
+
+        public static void merge(int[] origArr, int tempArr[], int beginIdx, int midIdx, int endIdx) {
+            int i = beginIdx, j = midIdx + 1;
+            int t = 0;
+            while (i <= midIdx && j <= endIdx) {
+                if (origArr[i] < origArr[j]) {
+                    tempArr[t++] = origArr[i++];
+                } else {
+                    tempArr[t++] = origArr[j++];
+                }
+            }
+
+            while (i <= midIdx) {
+                tempArr[t++] = origArr[i++];
+            }
+            while (j <= endIdx) {
+                tempArr[t++] = origArr[j++];
+            }
+
+            // 把暂存的tempArr排序结果，反哺至原数组
+            t = 0;
+            while (beginIdx <= endIdx) {
+                origArr[beginIdx++] = tempArr[t++];
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] simpleArr = new int[] {10, 5, 0, 2, 3, 9, 6, 8, 4, 7, 0, 1};
-        quickSort(simpleArr, 0, simpleArr.length - 1);
 
-        LogUtils.info(simpleArr);
+        int[] arr1 = copyArray(simpleArr);
+        quickSort(arr1, 0, arr1.length - 1);
+        LogUtils.info("quickSort", arr1);
+
+        int[] arr2 = copyArray(simpleArr);
+        MergeSortRecurse.sortAndMerge(arr2, new int[arr2.length], 0, arr2.length - 1);
+        LogUtils.info("mergeSort", arr2);
+
+    }
+
+    private static int[] copyArray(int[] simpleArr) {
+        return Arrays.copyOf(simpleArr, simpleArr.length);
     }
 
 }
